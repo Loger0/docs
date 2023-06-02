ceb272a62c93a1d330f4647673a06cf5e99e83e2
roger <1351429226@qq.com>
2023-06-02 17:24:12 +0800

    deploy

---
+++ nav.js
@@ -0,0 +1,16 @@
+module.exports = [
+  { text: "首页", link: "/" },
+  { text: "掘金", link: "https://juejin.cn/user/1913628905977070" },
+  { text: "前端杂谈", link: "/Native/Promise/" },
+  {
+    text: "技术分类",
+    items: [
+      { text: "html", link: "/interview/html/" },
+      { text: "css", link: "/interview/css/" },
+      { text: "JS", link: "/interview/js/" },
+      { text: "Node", link: "/interview/node/" },
+      { text: "Vue", link: "/Vue/" },
+      { text: "浏览器", link: "/Chrome/" },
+    ],
+  },
+];
---
+++ sidebar.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "/Vue/": require("../../Vue/sidebar"),
+  "/Chrome/": require("../../Chrome/sidebar"),
+  "/interview/": require("../../interview/sidebar"),
+  "/Native/": require("../../Native/sidebar"),
+};
